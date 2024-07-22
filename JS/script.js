f24d0e7a17675c7ab3bccf066c83fd95d8081fd7
Your Name <you@example.com>
2024-07-22 03:45:38 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let chelsii = 20;
+let farengeit = (9/5) * chelsii + 32;
+console.log(farengeit);
+
+let dayInMonth = 28;
+let hoursInMonth = (dayInMonth * 24);
+let minutesInMonth = (hoursInMonth * 60);
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+let playerHealth = 100;
+let playerEnergy = 100;
+let playerHealthAfterFight = (playerHealth - 30);
+let playerEnergyAfterFight = (playerEnergy - 76);
+console.log(playerHealthAfterFight);
+console.log(playerEnergyAfterFight);
+
+let lotPrice = 170;
+let lotDiscount = (lotPrice/100) * 10;
+console.log(lotDiscount);
+
+console.log(Math.floor(12.7));
+
+let numberFloat = '12.314asfasg12';
+console.log(Number.parseFloat(numberFloat));
+
+let numberInt = '12.314asfasg12';
+console.log(Number.parseInt(numberInt));
+
+let numberSqrt = 144;
+console.log(Math.sqrt(numberSqrt));
+
+let numberIntString = '257.40idk1how3to4complete5this';
+console.log(Number.parseInt(numberIntString));
